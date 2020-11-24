a46fdcc7d799c49a8794c8f3aa173cfb3da993f6
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2020-11-24 17:05:59 +0100

    changed functions

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,57 @@
 console.log("hello")
+
+window.addEventListener("load", main);
+
+let character: HTMLElement | null;
+
+let position = { bottom: 0, left: 0 }
+
+function main() {
+    character = document.querySelector(".character");
+    addEventListeners();
+
+    runGameLoop();
+}
+
+function addEventListeners() {
+    window.addEventListener("keydown", handleUserInput);
+}
+
+function handleUserInput(event: KeyboardEvent) {
+    switch(event.key) {
+        case "ArrowLeft":
+            position.left -= 1;
+            break;
+
+        case "ArrowRight":
+            position.left += 1;
+            break;
+
+        case " ":
+            position.bottom += 10;
+            break;
+    }
+}
+
+function renderCharacter() {
+    if (character) {
+        character.style.bottom = position.bottom + "%";
+        character.style.left = position.left + "%"
+    }
+}
+
+
+function runGameLoop() {
+    setInterval(function() {
+        applyGravity();
+
+        if(position.bottom < 0) {
+            position.bottom = 0;
+        }
+        renderCharacter();
+    }, 100);
+}
+
+function applyGravity() {
+    position.bottom -= 1;
+}
